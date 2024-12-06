d4d93a3e3e0fe08cba646b44fd3ba77878366e64
DimaKhrapko <khrapko2007@gmail.com>
2024-12-06 23:56:20 +0200

    Add solutions

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const data = [];
+  const get = (i) => data[i];
+  get.push = (x) => data.push(x);
+  get.pop = (x) => data.pop(x);
+  return get;
+};
 
 module.exports = { array };
